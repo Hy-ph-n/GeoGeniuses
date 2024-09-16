0fe7140a9855d42ecc06af465ef09c7a5768d5aa
waveb <waveb@192.168.1.65>
2024-09-16 10:56:13 -0500

    The java program now uses a local database

--- src/geogeniuses/GeoGeniuses.java
+++ src/geogeniuses/GeoGeniuses.java
@@ -13,18 +13,20 @@ public class GeoGeniuses extends State {
     static Timer connectionTimer;
 
     //The sql link is created here.
-    static String mySQLURL = "jdbc:mysql://13.58.236.216:3306/dbowensu24";
-
-    static String userName = "dbowensu24";
-
-    static String password = "cpt_Tstc1";
+    static String sqliteURL = "jdbc:sqlite:geogeniuses.db";
 
     static int delay = 7500;
 
     public static void main(String[] args) {
 
         try {
-            con = DriverManager.getConnection(GeoGeniuses.mySQLURL, GeoGeniuses.userName, GeoGeniuses.password);
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException c) {
+            System.out.print(c);
+        }
+
+        try {
+            con = DriverManager.getConnection(sqliteURL);
         } catch (SQLException e) {
             System.out.println(e);
         }
@@ -47,45 +49,25 @@ public class GeoGeniuses extends State {
             public void actionPerformed(ActionEvent e) {
                 try {
                     delay = 2000;
-                    if (con.isValid(2)) {
-                        if (!con.isClosed()) {
-                            connectionStatus.setText("");
-                            LoginView.resetPasswordButton.setEnabled(true);
-                            ResetPassword.confirmAnswersButton.setEnabled(true);
-                            ResetPassword.confirmNewPasswordButton.setEnabled(true);
-                            Register.registerButton.setEnabled(true);
-                            CustomerView.checkout.setEnabled(true);
-                            ManagerView.addUser.setEnabled(true);
-                            ManagerView.editUser.setEnabled(true);
-                            ManagerView.deleteUser.setEnabled(true);
-                            ManagerView.addItem.setEnabled(true);
-                            ManagerView.addImage.setEnabled(true);
-                            ManagerView.editItem.setEnabled(true);
-                            ManagerView.deleteItem.setEnabled(true);
-                            ManagerView.addDiscount.setEnabled(true);
-                            ManagerView.editDiscount.setEnabled(true);
-                        } else {
-                            connectionStatus.setText("Connection closed");
-                            con = DriverManager.getConnection(mySQLURL, userName, password);
-
-                            LoginView.resetPasswordButton.setEnabled(false);
-                            ResetPassword.confirmAnswersButton.setEnabled(false);
-                            ResetPassword.confirmNewPasswordButton.setEnabled(false);
-                            Register.registerButton.setEnabled(false);
-                            CustomerView.checkout.setEnabled(false);
-                            ManagerView.addUser.setEnabled(false);
-                            ManagerView.editUser.setEnabled(false);
-                            ManagerView.deleteUser.setEnabled(false);
-                            ManagerView.addItem.setEnabled(false);
-                            ManagerView.addImage.setEnabled(false);
-                            ManagerView.editItem.setEnabled(false);
-                            ManagerView.deleteItem.setEnabled(false);
-                            ManagerView.addDiscount.setEnabled(false);
-                            ManagerView.editDiscount.setEnabled(false);
-                        }
+                    if (!con.isClosed()) {
+                        connectionStatus.setText("");
+                        LoginView.resetPasswordButton.setEnabled(true);
+                        ResetPassword.confirmAnswersButton.setEnabled(true);
+                        ResetPassword.confirmNewPasswordButton.setEnabled(true);
+                        Register.registerButton.setEnabled(true);
+                        CustomerView.checkout.setEnabled(true);
+                        ManagerView.addUser.setEnabled(true);
+                        ManagerView.editUser.setEnabled(true);
+                        ManagerView.deleteUser.setEnabled(true);
+                        ManagerView.addItem.setEnabled(true);
+                        ManagerView.addImage.setEnabled(true);
+                        ManagerView.editItem.setEnabled(true);
+                        ManagerView.deleteItem.setEnabled(true);
+                        ManagerView.addDiscount.setEnabled(true);
+                        ManagerView.editDiscount.setEnabled(true);
                     } else {
-                        connectionStatus.setText("Invalid connection");
-                        con = DriverManager.getConnection(mySQLURL, userName, password);
+                        connectionStatus.setText("Connection closed");
+                        con = DriverManager.getConnection(sqliteURL);
 
                         LoginView.resetPasswordButton.setEnabled(false);
                         ResetPassword.confirmAnswersButton.setEnabled(false);
@@ -105,7 +87,7 @@ public class GeoGeniuses extends State {
                 } catch (SQLException ex) {
                     connectionStatus.setText("Connection error");
                     try {
-                        con = DriverManager.getConnection(mySQLURL, userName, password);
+                        con = DriverManager.getConnection(sqliteURL);
                     } catch (SQLException exe) {
                         LoginView.resetPasswordButton.setEnabled(false);
                         ResetPassword.confirmAnswersButton.setEnabled(false);
